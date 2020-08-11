da1b0a2edd5f10b205c695166154516ae520a476
Prakshl <61313182+Prakshl@users.noreply.github.com>
2020-08-11 17:51:07 +0530

    Update Locators.py

--- Locators/Locators.py
+++ Locators/Locators.py
@@ -22,7 +22,8 @@ privacy_policy = "//android.view.View[@index='6']"
 licence_agreement = "//android.view.View[@index='7']"
 i_agree = "//android.widget.Button[@text='I Agree']"
 
-profile = "com.bose.bosemusic:id/item_account_settings"
+# profile = "com.bose.bosemusic:id/item_account_settings"
+profile = ('//*[@class="android.widget.TextView"])[1]'
 sign_out = "com.bose.bosemusic:id/button"
 
 allow_notification = "com.bose.bosemusic:id/primary_button"

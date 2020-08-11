76440da1076afebd2f3764fc0bc1d7db3ed9ae7a
Prakshl <61313182+Prakshl@users.noreply.github.com>
2020-08-11 16:48:05 +0530

    change no of times dropdwn to be swipe

--- Methods/signin_signup_methods.py
+++ Methods/signin_signup_methods.py
@@ -164,8 +164,8 @@ class Sign_up:
             
             touch = TouchAction(drivers)
 
-            # Looping elements until India not shown into frame (15 times)
-            for i in range(12):
+            # Looping elements until India not shown into frame (18 times)
+            for i in range(18):
                 drivers.swipe(100,700,100,150)
 #                 touch.press(x=492, y=1055).move_to(x=492, y=771).release().perform()
                 time.sleep(2)

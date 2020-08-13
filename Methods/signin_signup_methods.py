3c4cdea7902a4f4eabbb60d8db060ed012c047f8
Prakshl <61313182+Prakshl@users.noreply.github.com>
2020-08-13 14:28:33 +0530

    Update signin_signup_methods.py

--- Methods/signin_signup_methods.py
+++ Methods/signin_signup_methods.py
@@ -200,7 +200,7 @@ class Sign_up:
             drivers.find_element_by_xpath(licence_agreement).click()
 
             # I Agree
-            drivers.find_element_by_id(i_agree).click()
+            drivers.find_elements_by_accessibility_id(i_agree).click()
 
             return True
 

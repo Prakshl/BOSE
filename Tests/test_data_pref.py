fc88e51bddef46c55b6ff68bbbe3d0e602643847
Prakshal Shah <prakshal.shah@EIC.EINFOCHIPS.COM>
2020-08-19 11:44:06 +0530

    Added new test file

---
+++ Tests/test_data_pref.py
@@ -0,0 +1,47 @@
+from Methods.signin_signup_methods import *
+
+# Object of Login class
+login_obj = Login()
+
+# Object of Common methods class
+common_obj = common_methods()
+
+
+# Calling Desired Caps method
+def test_desired_capabilities(platformname, udid, platformversion):
+    assert common_obj.desired_caps(platformname, udid, platformversion)
+
+
+# Calling method to click on signin and sign with email
+def test_login_btn():
+    assert login_obj.sign_in_btn()
+    time.sleep(3)
+
+
+# Calling method to write Login details
+def test_login_fill():
+    assert login_obj.fill_login_details()
+    time.sleep(25)
+
+
+# Calling method to Allow all required permission
+def test_allow_all_permission():
+    assert common_obj.allow_permissions()
+    time.sleep(5)
+
+
+# Calling method to open profile
+def test_open_my_profile():
+    assert common_obj.open_profile()
+    time.sleep(4)
+
+
+# Calling method to turn on or off data preference
+def test_data_pref():
+    assert common_obj.data_preference()
+    time.sleep(3)
+
+
+# Signout method
+def test_sign_out():
+    assert common_obj.sign_out_btn()

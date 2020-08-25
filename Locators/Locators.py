14524c3569a8ebd7cba739ed1fbe56995848b8dd
Prakshl <61313182+Prakshl@users.noreply.github.com>
2020-08-25 10:52:04 +0530

    Update profile locator

--- Locators/Locators.py
+++ Locators/Locators.py
@@ -21,8 +21,8 @@ i_agree = 'gigya-privacy-accept'
 
 
 
-# profile = '//android.widget.TextView[@content-desc="Bose Account, button"]'
-profile = '(//*[@class="android.widget.TextView"])[1]'
+profile = '//android.widget.TextView[@content-desc="Bose Account, button"]'
+# profile = '(//*[@class="android.widget.TextView"])[1]'
 # profile = 'Bose Account, button'
 
 # account = '(//*[@class="android.view.ViewGroup"])[1]'

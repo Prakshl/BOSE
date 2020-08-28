aae14483980bdd1235a1ceee672eebb1093fba84
Prakshal Shah <prakshal.shah@EIC.EINFOCHIPS.COM>
2020-08-28 16:09:28 +0530

    update if else loop for profile

--- Methods/signin_signup_methods.py
+++ Methods/signin_signup_methods.py
@@ -50,12 +50,15 @@ class common_methods:
         try:
             # Opening the profile
             if drivers.find_element_by_id(profile):
+                print("Found element 1")
                 drivers.find_element_by_id(profile).click()
 
             elif drivers.find_element_by_xpath(profile1):
+                print("Found element 2")
                 drivers.find_element_by_xpath(profile).click()
 
             elif drivers.find_element_by_xpath(profile2):
+                print("Found element 3")
                 drivers.find_element_by_xpath(profile2).click()
 
             else:

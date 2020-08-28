dfd696a9237df44d7f0f6e014a34302d615c0a84
Prakshal Shah <prakshal.shah@EIC.EINFOCHIPS.COM>
2020-08-28 18:01:20 +0530

    update in profile

--- Methods/signin_signup_methods.py
+++ Methods/signin_signup_methods.py
@@ -50,7 +50,7 @@ class common_methods:
         try:
             # Opening the profile
             print("PROFILE")
-            drivers.find_element_by_xpath(profile1).click()
+            drivers.find_element_by_xpath(profile2).click()
             screenshot()
             return True
 

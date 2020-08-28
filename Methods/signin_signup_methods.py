0ecc69a6801cf8f7e65086e9532bf5acd98c67d4
Prakshal Shah <prakshal.shah@EIC.EINFOCHIPS.COM>
2020-08-28 16:33:13 +0530

    update in profile

--- Methods/signin_signup_methods.py
+++ Methods/signin_signup_methods.py
@@ -49,22 +49,8 @@ class common_methods:
 
         try:
             # Opening the profile
-            if drivers.find_element_by_id(profile).size > 0:
-                print("Found element 1")
-                drivers.find_element_by_id(profile).click()
-
-            elif drivers.find_element_by_xpath(profile1).size > 0:
-                print("Found element 2")
-                drivers.find_element_by_xpath(profile).click()
-
-            elif drivers.find_element_by_xpath(profile2).size > 0:
-                print("Found element 3")
-                drivers.find_element_by_xpath(profile2).click()
-
-            else:
-                print("No element found")
-
-            # drivers.find_element_by_id(profile).click()
+            drivers.find_element_by_xpath(profile1).click()
+            screenshot()
             return True
 
         except (NoSuchElementException, WebDriverException, RemoteDriverServerException):
